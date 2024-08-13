ceb279ba98d390e3a47c672a8ee80d731be255c1
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-08-14 01:14:18 +0530

    Refactor API routes for pagination and limit

--- index.js
+++ index.js
@@ -344,7 +344,7 @@ app.post('/api/books', async (req, res) => {
 //   }
 // });
 
-app.get('/api/bookss', async (req, res) => {
+app.get('/api/books', async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 5;
   const offset = (page - 1) * limit;
